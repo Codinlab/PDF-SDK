647c24d5caf30ee988095739443da9bdbf39933c
Thierry Fleury <thierry@codinlab.fr>
2016-12-20 17:53:20 +0100

    fixup! Add test project

--- MyGet.ps1
+++ MyGet.ps1
@@ -6,6 +6,8 @@ $versionSuffix = "CI{0:D4}" -f [int]$env:BuildCounter
 dotnet restore
 dotnet build
 
+dotnet test test\DocumentFormat.Pdf.Tests
+
 if($?) {
 	dotnet pack src/DocumentFormat.Pdf --output $nupkgsFolder --version-suffix $versionSuffix
 }
