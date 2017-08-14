607361aee4bb788ba4c7c551b4c5735d5127e667
Thierry Fleury <thierry@codinlab.fr>
2017-08-14 11:00:15 +0200

    Add Functional tests

---
+++ test/DocumentFormat.Pdf.FunctionalTests/PdfDocumentTests.cs
@@ -0,0 +1,33 @@
+using Microsoft.Extensions.PlatformAbstractions;
+using System.IO;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace DocumentFormat.Pdf.FunctionalTests
+{
+    public class PdfDocumentTests
+    {
+        private static string SampleFilesPath {
+            get => Path.Combine(PlatformServices.Default.Application.ApplicationBasePath, "TestFiles");
+        }
+
+        [Theory]
+        [InlineData("AdobeAcrobat.pdf")]
+        [InlineData("WordGenerated.pdf")]
+        public void OpensSampleFile(string fileName)
+        {
+            // Arrange
+            PdfDocument doc;
+
+            // Act
+            using (var fs = File.OpenRead(Path.Combine(SampleFilesPath, fileName)))
+            {
+                doc = PdfDocument.Open(fs);
+            }
+
+            // Assert
+            Assert.NotNull(doc);
+        }
+
+    }
+}
