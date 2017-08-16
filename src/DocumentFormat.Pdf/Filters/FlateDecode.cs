64ad82b875f904ed07517355d3ed593cca5c7a6d
Thierry Fleury <thierry@codinlab.fr>
2017-08-16 10:23:55 +0200

    PdfFilter base class and first test of FlateDecode

---
+++ src/DocumentFormat.Pdf/Filters/FlateDecode.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Text;
+
+namespace DocumentFormat.Pdf.Filters
+{
+    /// <summary>
+    /// Represents the FlateDecode filter.
+    /// </summary>
+    public class FlateDecode : PdfFilter
+    {
+        /// <summary>
+        /// FlateDecode filter name.
+        /// </summary>
+        public override string Name => "FlateDecode";
+
+        /// <summary>
+        /// Decodes data using FlateDecode filter.
+        /// </summary>
+        /// <param name="data">The data to decode.</param>
+        /// <returns>Decoded data.</returns>
+        public override byte[] Decode(byte[] data)
+        {
+            if (data == null)
+                throw new ArgumentNullException(nameof(data));
+
+            using (var inputStream = new MemoryStream(data))
+            {
+                var header = new byte[2];
+                inputStream.Read(header, 0, header.Length);
+                using (var outputStream = new MemoryStream())
+                {
+                    using (var deflateStream = new DeflateStream(inputStream, CompressionMode.Decompress))
+                    {
+                        deflateStream.CopyTo(outputStream);
+                    }
+
+                    return outputStream.ToArray();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Encodes data using FlateDecode filter.
+        /// </summary>
+        /// <param name="data">The data to encode.</param>
+        /// <returns>Encoded data.</returns>
+        public override byte[] Encode(byte[] data)
+        {
+            if (data == null)
+                throw new ArgumentNullException(nameof(data));
+
+            using (var inputStream = new MemoryStream(data))
+            {
+                using (var outputStream = new MemoryStream())
+                {
+                    using (var deflateStream = new DeflateStream(outputStream, CompressionMode.Compress))
+                    {
+                        inputStream.CopyTo(deflateStream);
+                    }
+
+                    return outputStream.ToArray();
+                }
+            }
+        }
+    }
+}
---
+++ src/DocumentFormat.Pdf/Filters/PdfFilter.cs
@@ -0,0 +1,27 @@
+﻿namespace DocumentFormat.Pdf.Filters
+{
+    /// <summary>
+    /// Represents PDF Filter base class
+    /// </summary>
+    public abstract class PdfFilter
+    {
+        /// <summary>
+        /// Filter's Name
+        /// </summary>
+        public abstract string Name { get; }
+
+        /// <summary>
+        /// When implemented in a derived class encodes the specified data.
+        /// </summary>
+        /// <param name="data">The byte array to encode.</param>
+        /// <returns>Encoded data.</returns>
+        public abstract byte[] Encode(byte[] data);
+
+        /// <summary>
+        /// When implemented in a derived class decodes the specified data.
+        /// </summary>
+        /// <param name="data">The byte array to decode.</param>
+        /// <returns>Decoded data.</returns>
+        public abstract byte[] Decode(byte[] data);
+    }
+}
