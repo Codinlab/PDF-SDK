1e297a1a3799a9123b83bddef4fe5d69db5b0408
Thierry Fleury <thierry@codinlab.fr>
2017-08-21 14:12:13 +0200

    Add PdfWriterExtensions

---
+++ src/DocumentFormat.Pdf/Extensions/PdfWriterExtensions.cs
@@ -0,0 +1,32 @@
+﻿using DocumentFormat.Pdf.IO;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DocumentFormat.Pdf.Extensions
+{
+    /// <summary>
+    /// <see cref="PdfWriter"/> extension methods.
+    /// </summary>
+    public static class PdfWriterExtensions
+    {
+        /// <summary>
+        /// Writes a comment to the PDF stream.
+        /// </summary>
+        /// <param name="writer">The <see cref="PdfWriter"/> to use.</param>
+        /// <param name="comment">The comment to write.</param>
+        public static void WriteComment(this PdfWriter writer, string comment)
+        {
+            if (writer == null)
+                throw new ArgumentNullException(nameof(writer));
+
+            var lines = comment.Split(new char[] { Chars.CR, Chars.LF }, StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (var line in lines)
+            {
+                writer.Write('%');
+                writer.WriteLine(line);
+            }
+        }
+    }
+}
